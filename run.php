1bb4ad445839683a1448eeec138fd4797b231fb8
anepedekate <58905332+anepedekate@users.noreply.github.com>
2021-06-05 09:45:51 +0700

    anepedekate run.php
    git pull

--- run.php
+++ run.php
@@ -30,7 +30,7 @@ while($i<$jumlah){
 function news($uid,$n){ 
 		$rand = rand(1,9999);
 		$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, "https://ai.caping.co.id/v2/event/news/view/$rand"); 
+			curl_setopt($ch, CURLOPT_URL, "https://ai.caping.co.id/v5.4.1/event/news/view/$rand"); 
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 				$headers = array();
@@ -43,7 +43,7 @@ function news($uid,$n){
 function video($uid,$n){
 		$rand = rand(1,9999);
 		$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, "https://ai.caping.co.id/v2/event/video/view/$rand"); 
+			curl_setopt($ch, CURLOPT_URL, "https://ai.caping.co.id/v5.4.1/event/video/view/$rand"); 
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 				$headers = array();
@@ -57,7 +57,7 @@ function video($uid,$n){
 function share($uid,$n){
 		$rand = rand(1,9999);
 		$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, "https://ai.caping.co.id/v2/event/share/news/$rand"); 
+			curl_setopt($ch, CURLOPT_URL, "https://ai.caping.co.id/v5.4.1/event/share/news/$rand"); 
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 				$headers = array();
@@ -70,7 +70,7 @@ function share($uid,$n){
 }
 function code($uid,$n){
 		$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, "https://ai.caping.co.id/v2/event/share/code"); 
+			curl_setopt($ch, CURLOPT_URL, "https://ai.caping.co.id/v5.4.1/event/share/code"); 
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 				$headers = array();
@@ -83,7 +83,7 @@ function code($uid,$n){
 }
 function klik($uid,$n){
 		$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, "https://ai.caping.co.id/v2/event/share/click/push"); 
+			curl_setopt($ch, CURLOPT_URL, "https://ai.caping.co.id/v5.4.1/event/share/click/push"); 
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 			$headers = array();
